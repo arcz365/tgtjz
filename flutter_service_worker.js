9eaf11ba7e4468ffcb9f2da4a8d70e88ee8d0c8b
JAVADZ <JAVAD.ARCZ365@GMAIL.COM>
2025-10-13 23:48:16 +1100

    first post

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "efe1ed78d9b2b681b73d8ec97a09ecc1",
+const RESOURCES = {"flutter_bootstrap.js": "556ea5a8648b7f44eb4630cc746a67c9",
 "version.json": "9e2dbd00f7cd683d82814bf2d99ca889",
 "index.html": "8b18411f03a0a89b8f6c1106d7f1bdc8",
 "/": "8b18411f03a0a89b8f6c1106d7f1bdc8",
-"main.dart.js": "cf2f25fbddfd2919f3ae881434d3e395",
+"main.dart.js": "6c8877746224dd706450878c0bae7d59",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
